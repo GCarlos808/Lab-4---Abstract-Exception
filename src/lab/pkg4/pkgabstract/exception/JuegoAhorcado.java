4e22b887fa5710faca9612500416a8a408c5951a
Rafael Busmail <rafaelbusmail12@gmail.com>
2026-02-13 09:31:03 -0600

    interfaz 1.3

---
+++ src/lab/pkg4/pkgabstract/exception/JuegoAhorcado.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    package lab.pkg4.pkgabstract.exception;
+    public interface JuegoAhorcado {
+       void inicializarPalabraSecreta();
+       void jugar();
+
+    }
